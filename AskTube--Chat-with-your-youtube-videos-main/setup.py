db1f3d9a8dfd584860914a47c01c130f958a65c8
Aniket Ashok Sarap <aniketsarap27@gmail.com>
2025-09-30 16:55:23 +0100

    Update setup.py

--- AskTube--Chat-with-your-youtube-videos-main/setup.py
+++ AskTube--Chat-with-your-youtube-videos-main/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='chat_with_video',
     version='0.0.1',
-    author='saurabh naik',
-    author_email='naiksaurabhd@gmail.com',
+    author='aniket sarap',
+    author_email='aniketsarap27@gmail.com',
     install_requires=["langchain","streamlit","python-dotenv","PyPDF2","google-generativeai"],
     packages=find_packages()
 )
